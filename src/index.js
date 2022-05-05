e279bbf661a9dfbcabdfb2dde16df87ed5f0ebf1
Ulukbek Kasymov <ulik.kasymoff@gmail.com>
2022-05-06 02:11:51 +0600

    feat: add input to textarea from keyboard

--- src/index.js
+++ src/index.js
@@ -11,17 +11,18 @@ function component(component, id, options) {
 document.body.appendChild(component('h1', 'title'));
 document.querySelector('#title').textContent = "Virtual Keyboard";
 document.body.appendChild(component('textarea', "text-area"));
+// document.querySelector("#text-area").setAttribute('disabled', 'true');
 document.body.appendChild(component('div', "keyboard"));
 document.querySelector("#keyboard").appendChild(component('div', "keyboard-keys"));
 
-const keyboardENlc = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', "Delete", 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '⮝', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Control'];
-const keyboardENuc = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', "Delete", 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '⮝', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Control'];
-const keyboardRUlc = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', "Delete", 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '⮝', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Control'];
-const keyboardRUuc = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', "Delete", 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '⮝', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Control'];
-const keyboardENlcWithShift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', '|', "Delete", 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', '⮝', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Control'];
-const keyboardENucWithShift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', "Delete", 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '⮝', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Control'];
-const keyboardRUlcWithShift = ['ё', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', "Delete", 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ',', '⮝', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Control'];
-const keyboardRUucWithShift = ['Ё', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '|', "Delete", 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '⮝', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Control'];
+const keyboardENlc = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', "Delete", 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '⮝', 'Shift', 'Ctrl', '⊞', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Ctrl'];
+const keyboardENuc = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', "Delete", 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '⮝', 'Shift', 'Ctrl', '⊞', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Ctrl'];
+const keyboardRUlc = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', "Delete", 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '⮝', 'Shift', 'Ctrl', '⊞', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Ctrl'];
+const keyboardRUuc = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', "Delete", 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '⮝', 'Shift', 'Ctrl', '⊞', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Ctrl'];
+const keyboardENlcWithShift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', '|', "Delete", 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', '⮝', 'Shift', 'Ctrl', '⊞', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Ctrl'];
+const keyboardENucWithShift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', "Delete", 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '⮝', 'Shift', 'Ctrl', '⊞', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Ctrl'];
+const keyboardRUlcWithShift = ['ё', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', "Delete", 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ',', '⮝', 'Shift', 'Ctrl', '⊞', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Ctrl'];
+const keyboardRUucWithShift = ['Ё', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '|', "Delete", 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '⮝', 'Shift', 'Ctrl', '⊞', 'Alt', ' ', 'Alt', '⮜', '⮟', '⮞', 'Ctrl'];
 
 const codes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 let keysPressed = {};
@@ -50,9 +51,14 @@ if (loadedLayout === 'й') {
 
 localStorage.setItem('layout', loadedLayout);
 
+document.querySelector('#text-area').oninput = (event) => {
+  
+}
+
 document.onkeydown = (event) => {
+  let inputStart = document.querySelector('#text-area').selectionStart;
   keysPressed[event.code] = true;
-  if (event.code === "Tab" || event.code === "MetaLeft" || event.code === "AltLeft" || event.code === "AltRight") {
+  if (event.code === "Tab" || event.code === "MetaLeft" || event.code === "AltLeft" || event.code === "AltRight" || event.code === "ShiftLeft" || event.code === "ShiftRight") {
     event.preventDefault();
   }
   if (keysPressed['AltLeft'] && event.code === 'ShiftLeft' || keysPressed['AltLeft'] && event.code === 'ShiftRight' || keysPressed['AltRight'] && event.code === 'ShiftRight' || keysPressed['AltRight'] && event.code === 'ShiftLeft') {
@@ -69,7 +75,6 @@ document.onkeydown = (event) => {
       init(keyboardENlc);
       localStorage.setItem('layout', loadedLayout)
     }
-    console.log(localStorage)
   } else if (event.code === 'CapsLock') {
     if (loadedLayout === 'q') {
       init(keyboardENuc);
@@ -88,7 +93,6 @@ document.onkeydown = (event) => {
       localStorage.setItem('layout', loadedLayout)
       document.querySelector('#CapsLock').classList.remove('active');
     }
-    console.log(localStorage)
   } else if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
     if (loadedLayout === 'q') {
       init(keyboardENucWithShift);
@@ -99,9 +103,50 @@ document.onkeydown = (event) => {
     } else if (loadedLayout === 'Й') {
       init(keyboardRUlcWithShift);
     }
-    console.log(localStorage)
     document.querySelector(`#${event.code}`).classList.add('active');
   } else {
+    if (event.code !== 'Backspace' && event.code !== 'Tab' && event.code !== 'Delete' && event.code !== 'CapsLock' 
+    && event.code !== 'Enter' && event.code !== 'ShiftLeft' && event.code !== 'ShiftRight' && event.code !== 'ControlLeft' 
+    && event.code !== 'ControlRight' && event.code !== 'AltLeft' && event.code !== 'AltRight' && event.code !== 'MetaLeft') {
+      event.preventDefault();
+      const val = document.querySelector(`#${event.code}`).textContent;
+      let arr = document.querySelector('#text-area').value.split('');
+      arr.splice(inputStart, 0, val);
+      document.querySelector('#text-area').value = arr.join('');
+      inputStart++;
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.code === 'Backspace') {
+      event.preventDefault();
+      if (inputStart > 0) {
+        let arr = document.querySelector('#text-area').value.split('');
+        arr.splice(inputStart - 1, 1);
+        document.querySelector('#text-area').value = arr.join('');
+        inputStart--;
+      }
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.code === 'Tab') {
+      event.preventDefault();
+      const val = '\t';
+      let arr = document.querySelector('#text-area').value.split('');
+      arr.splice(inputStart, 0, val);
+      document.querySelector('#text-area').value = arr.join('');
+      inputStart++;
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.code === 'Enter') {
+      event.preventDefault();
+      const val = '\n';
+      let arr = document.querySelector('#text-area').value.split('');
+      arr.splice(inputStart, 0, val);
+      document.querySelector('#text-area').value = arr.join('');
+      inputStart++;
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.code === 'Delete') {
+      event.preventDefault();
+      let arr = document.querySelector('#text-area').value.split('');
+      arr.splice(inputStart, 1);
+      document.querySelector('#text-area').value = arr.join('');
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    }
     document.querySelector(`#${event.code}`).classList.add('active');
   }
 }
@@ -110,6 +155,8 @@ document.onkeyup = (event) => {
   delete keysPressed[event.code];
   if (event.code === "CapsLock") {
     null;
+  } else if (keysPressed['AltLeft'] && event.code === 'ShiftLeft' || keysPressed['AltLeft'] && event.code === 'ShiftRight' || keysPressed['AltRight'] && event.code === 'ShiftRight' || keysPressed['AltRight'] && event.code === 'ShiftLeft') {
+
   } else if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
     if (loadedLayout === 'Q') {
       if (prevLayout === 'q') {
@@ -128,9 +175,14 @@ document.onkeyup = (event) => {
         localStorage.setItem('layout', loadedLayout)
       }
     }
-    console.log(localStorage)
     document.querySelector(`#${event.code}`).classList.remove('active');
   } else {
     document.querySelector(`#${event.code}`).classList.remove('active');
+  }
+}
+
+document.querySelector('#keyboard-keys').onclick = (event) => {
+  if (event.target.classList.contains("key")) {
+    
   }
 }
