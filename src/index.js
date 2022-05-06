6e9ced03b33b27aa602af91ba77e7e4faae92ddd
Ulukbek Kasymov <ulik.kasymoff@gmail.com>
2022-05-07 01:30:43 +0600

    feat: add mouseinput

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import "./style.scss";
 
 function component(component, id, options) {
   const element = document.createElement(component, options);
-
   element.setAttribute("id", id);
-  
   return element;
 }
 
 document.body.appendChild(component('h1', 'title'));
 document.querySelector('#title').textContent = "Virtual Keyboard";
 document.body.appendChild(component('textarea', "text-area"));
-// document.querySelector("#text-area").setAttribute('disabled', 'true');
 document.body.appendChild(component('div', "keyboard"));
 document.querySelector("#keyboard").appendChild(component('div', "keyboard-keys"));
 
@@ -28,6 +25,7 @@ const codes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'D
 let keysPressed = {};
 let loadedLayout = localStorage.getItem('layout');
 let prevLayout = '';
+let inputStart;
 
 function init(layout) {
   let out = '';
@@ -56,7 +54,7 @@ document.querySelector('#text-area').oninput = (event) => {
 }
 
 document.onkeydown = (event) => {
-  let inputStart = document.querySelector('#text-area').selectionStart;
+  inputStart = document.querySelector('#text-area').selectionStart;
   keysPressed[event.code] = true;
   if (event.code === "Tab" || event.code === "MetaLeft" || event.code === "AltLeft" || event.code === "AltRight" || event.code === "ShiftLeft" || event.code === "ShiftRight") {
     event.preventDefault();
@@ -155,8 +153,6 @@ document.onkeyup = (event) => {
   delete keysPressed[event.code];
   if (event.code === "CapsLock") {
     null;
-  } else if (keysPressed['AltLeft'] && event.code === 'ShiftLeft' || keysPressed['AltLeft'] && event.code === 'ShiftRight' || keysPressed['AltRight'] && event.code === 'ShiftRight' || keysPressed['AltRight'] && event.code === 'ShiftLeft') {
-
   } else if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
     if (loadedLayout === 'Q') {
       if (prevLayout === 'q') {
@@ -181,8 +177,126 @@ document.onkeyup = (event) => {
   }
 }
 
-document.querySelector('#keyboard-keys').onclick = (event) => {
+document.querySelector('#keyboard-keys').onmousedown = (event) => {
+  inputStart = document.querySelector('#text-area').selectionStart;
   if (event.target.classList.contains("key")) {
-    
+    document.querySelector(`#${event.target.id}`).classList.add('active');
+    if (event.target.id !== 'Backspace' && event.target.id !== 'Tab' && event.target.id !== 'Delete' && event.target.id !== 'CapsLock' 
+    && event.target.id !== 'Enter' && event.target.id !== 'ShiftLeft' && event.target.id !== 'ShiftRight' && event.target.id !== 'ControlLeft' 
+    && event.target.id !== 'ControlRight' && event.target.id !== 'AltLeft' && event.target.id !== 'AltRight' && event.target.id !== 'MetaLeft') {
+      event.preventDefault();
+      const val = document.querySelector(`#${event.target.id}`).textContent;
+      let arr = document.querySelector('#text-area').value.split('');
+      arr.splice(inputStart, 0, val);
+      document.querySelector('#text-area').value = arr.join('');
+      inputStart++;
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.target.id === 'Backspace') {
+      event.preventDefault();
+      if (inputStart > 0) {
+        let arr = document.querySelector('#text-area').value.split('');
+        arr.splice(inputStart - 1, 1);
+        document.querySelector('#text-area').value = arr.join('');
+        inputStart--;
+      }
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.target.id === 'Tab') {
+      event.preventDefault();
+      const val = '\t';
+      let arr = document.querySelector('#text-area').value.split('');
+      arr.splice(inputStart, 0, val);
+      document.querySelector('#text-area').value = arr.join('');
+      inputStart++;
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.target.id === 'Enter') {
+      event.preventDefault();
+      const val = '\n';
+      let arr = document.querySelector('#text-area').value.split('');
+      arr.splice(inputStart, 0, val);
+      document.querySelector('#text-area').value = arr.join('');
+      inputStart++;
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.target.id === 'Delete') {
+      event.preventDefault();
+      let arr = document.querySelector('#text-area').value.split('');
+      arr.splice(inputStart, 1);
+      document.querySelector('#text-area').value = arr.join('');
+      document.querySelector('#text-area').setSelectionRange(inputStart, inputStart);
+    } else if (event.target.id === 'ShiftLeft' || event.target.id === 'ShiftRight') {
+      if (loadedLayout === 'q') {
+        init(keyboardENucWithShift);
+      } else if (loadedLayout === 'Q') {
+        init(keyboardENlcWithShift);
+      } else if (loadedLayout === 'й') {
+        init(keyboardRUucWithShift);
+      } else if (loadedLayout === 'Й') {
+        init(keyboardRUlcWithShift);
+      }
+    } else if (event.target.id === 'CapsLock') {
+      document.querySelector(`#${event.target.id}`).classList.toggle('active');
+      if (loadedLayout === 'q') {
+        init(keyboardENuc);
+        localStorage.setItem('layout', loadedLayout)
+        document.querySelector('#CapsLock').classList.add('active');
+      } else if (loadedLayout === 'Q') {
+        init(keyboardENlc);
+        localStorage.setItem('layout', loadedLayout)
+        document.querySelector('#CapsLock').classList.remove('active');
+      } else if (loadedLayout === 'й') {
+        init(keyboardRUuc);
+        localStorage.setItem('layout', loadedLayout)
+        document.querySelector('#CapsLock').classList.add('active');
+      } else if (loadedLayout === 'Й') {
+        init(keyboardRUlc);
+        localStorage.setItem('layout', loadedLayout)
+        document.querySelector('#CapsLock').classList.remove('active');
+      }
+    }
+  }
+}
+
+document.querySelector('#keyboard-keys').onmouseup = (event) => {
+  document.querySelector(`#${event.target.id}`).classList.remove('active');
+  if (event.target.id === 'ShiftLeft' || event.target.id === 'ShiftRight') {
+    if (loadedLayout === 'Q') {
+      if (prevLayout === 'q') {
+        init(keyboardENlc);
+        localStorage.setItem('layout', loadedLayout)
+      } else {
+        init(keyboardENuc);
+        localStorage.setItem('layout', loadedLayout)
+      } 
+    } else if (loadedLayout === 'Й') {
+      if (prevLayout === 'й') {
+        init(keyboardRUlc);
+        localStorage.setItem('layout', loadedLayout)
+      } else {
+        init(keyboardRUuc);
+        localStorage.setItem('layout', loadedLayout)
+      }
+    }
+  }
+}
+
+document.querySelector('#keyboard-keys').onmouseout = (event) => {
+  document.querySelector(`#${event.target.id}`).classList.remove('active');
+  if (event.target.id === 'ShiftLeft' || event.target.id === 'ShiftRight') {
+    if (loadedLayout === 'Q') {
+      if (prevLayout === 'q') {
+        init(keyboardENlc);
+        localStorage.setItem('layout', loadedLayout)
+      } else {
+        init(keyboardENuc);
+        localStorage.setItem('layout', loadedLayout)
+      } 
+    } else if (loadedLayout === 'Й') {
+      if (prevLayout === 'й') {
+        init(keyboardRUlc);
+        localStorage.setItem('layout', loadedLayout)
+      } else {
+        init(keyboardRUuc);
+        localStorage.setItem('layout', loadedLayout)
+      }
+    }
   }
 }
